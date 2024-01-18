cb8c6fdb9df7421c3efc95cc73e963b26b3cf7ee
rohit rawat <rohitrawatop@gmail.com>
2024-01-18 12:32:02 +0530

    session check

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,5 +16,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/user/:path*", '/dashboard/:path*'],
+  matcher: ["/user/:path*", '/dashboard/:path*', '/checkout/address'],
 }
