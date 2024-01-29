cd182545c658f8693aa7df14f3700ca6b694e7d0
rohit rawat <rohitrawatop@gmail.com>
2024-01-29 14:16:08 +0530

    env updated

--- src/lib/razorpay.ts
+++ src/lib/razorpay.ts
@@ -1,9 +1,9 @@
-
+'use server'
 import Razorpay from 'razorpay';
 
-export const razorpay = new Razorpay({
-  key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY || "",
-  key_secret: process.env.NEXT_PUBLIC_RAZORPAY_SECRET,
+const razorpayClient = new Razorpay({
+  key_id: process.env.RAZORPAY_API_KEY || "",
+  key_secret: process.env.RAZORPAY_API_SECRET,
 });
 
 export const createRazorpayOrder = async (amount: number, currency: any) => {
@@ -13,13 +13,10 @@ export const createRazorpayOrder = async (amount: number, currency: any) => {
     receipt: 'order_receipt', // You can customize the receipt ID as needed
   };
 
-  return new Promise((resolve, reject) => {
-    razorpay.orders.create(options, (err, order) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(order);
-      }
-    });
-  });
+  try {
+    const order = await razorpayClient.orders.create(options);
+    return order;
+  } catch (error) {
+    throw error; // Re-throw the error for proper handling
+  }
 };
