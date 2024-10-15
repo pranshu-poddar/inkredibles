7ef780db884003220d688d8c6ec6e5278834c141
rohit rawat <rohitrawatop@gmail.com>
2024-10-15 18:29:16 +0530

    contact updated

--- src/components/contact-us/top-section.tsx
+++ src/components/contact-us/top-section.tsx
@@ -10,12 +10,6 @@ const TopSection = () => {
     <div className="px-10 gap-8 py-24 flex">
       <div className="w-1/2">
         <h3 className="text-2xl">Contact Us</h3>
-        <p className="mt-6 mb-4">
-          Claritas est etiam processus dynamicus, qui sequitur mutationem
-          consuetudium lectorum. Mirum est notare quam littera gothica, quam
-          nunc putamus parum claram anteposuerit litterarum formas human. qui
-          sequitur mutationem consuetudium lectorum. Mirum est notare quam
-        </p>
         <ul className="[&>*]:py-4 [&>*]:border-t ">
           <li className="flex items-center gap-4">
             <FaFax />
